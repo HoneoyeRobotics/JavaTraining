836f5869fa82f081e8583943386a7ad3f2bf1351
Kaeden <kaedenh9@hotmail.com>
2018-12-09 15:52:14 -0500

    created new project

---
+++ Students/KZH2018/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+
+public class RobotMap {
+  
+   public static final int elbowMotorCanID = 7;
+   public static final int leftArmWheelCanID = 9;
+   public static final int rightArmWheelCanID = 10;
+   public static final int armElbowEncoderAChannelDIO = 0;
+   public static final int armElbowEncoderBChannelDIO = 1;
+   public static final int armLimitSwitchDIO = 2;
+}
